2f071b4aaaf3ee39afd9fbd5ba67846b36281910
Samuli Suomi <smsuom@utu.fi>
2015-06-30 02:50:47 +0300

    Add scrolling background and movable biker

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	public float scrollSpeed;
+	public float interval;
+	public float limit;
+
+	private float distance;
+	private float spawn;
+
+	public GameObject[] backgroundSprites;
+
+	// Use this for initialization
+	void Start () {
+		spawn = limit + 4 * interval;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = scrollSpeed * Time.deltaTime;
+		//Debug.Log(spawn);
+
+		foreach (GameObject bg in backgroundSprites) 
+		{
+			bg.transform.Translate(Vector3.down * distance, Space.World);
+			if (bg.transform.position.y < limit) {
+				bg.transform.position = new Vector3(bg.transform.position.x, spawn, bg.transform.position.z);
+				Debug.Log(bg.transform.position.y);
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/Bike.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bike : MonoBehaviour {
+
+	public float speed;
+	public float xLimit;
+
+	private float distance;
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x > -xLimit) {
+			// Keyboard:
+			if (Input.GetKey("a")) {
+				MoveLeft(speed * Time.deltaTime);
+			}
+			// Touch:
+			if (Input.touchCount > 0) {
+				Touch lastTouch = Input.touches[Input.touches.Length - 1];
+				if (lastTouch.position.x < Screen.width / 2) {
+					MoveLeft(speed * Time.deltaTime);
+				}
+			}
+		}
+		if (transform.position.x < xLimit) {
+			// Keyboard:
+			if (Input.GetKey("d")) {
+				MoveRight(speed * Time.deltaTime);
+			}
+			// Touch:
+			if (Input.touchCount > 0) {
+				Touch lastTouch = Input.touches[Input.touches.Length - 1];
+				if (lastTouch.position.x > Screen.width / 2) {
+					MoveRight(speed * Time.deltaTime);
+				}
+			}
+		}
+	}
+
+	void MoveLeft(float distance) {
+		transform.Translate(Vector3.left * distance, Space.World);
+	}
+
+	void MoveRight(float distance) {
+		transform.Translate(Vector3.right * distance, Space.World);
+	}
+}
